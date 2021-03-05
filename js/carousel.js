a2e17d7b097a23ce0ba8c794d5a1fe21a7fa88fa
yhtomitcastle <75184508+yhtomitcastle@users.noreply.github.com>
2021-03-05 14:51:30 -0500

    Multi-image and responsive carousel
    Updated photo carousel on all pages so that it responsively adds photos according to device width.

---
+++ js/carousel.js
@@ -0,0 +1,31 @@
+$('#archives').on('slide.bs.carousel', function (e) {
+
+    /*
+
+    CC 2.0 License Iatek LLC 2018
+    Attribution required
+
+    Modified by Tim from:
+    https://codepen.io/timar/pen/mabyEg
+    
+    */
+
+
+    var $e = $(e.relatedTarget);
+    var idx = $e.index();
+    var itemsPerSlide = 7;
+    var totalItems = $('.carousel-item').length;
+    
+    if (idx >= totalItems-(itemsPerSlide-1)) {
+        var it = itemsPerSlide - (totalItems - idx);
+        for (var i=0; i<it; i++) {
+            // append slides to end
+            if (e.direction=="left") {
+                $('.carousel-item').eq(i).appendTo('.carousel-inner');
+            }
+            else {
+                $('.carousel-item').eq(0).appendTo('.carousel-inner');
+            }
+        }
+    }
+});
